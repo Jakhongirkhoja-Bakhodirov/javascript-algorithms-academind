b2170998bc18b8f947819810e73a4b3198260ca7
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-03-20 15:24:53 +0500

    isPrime() function is improved version then Big O(sqrt)

--- isPrime.js
+++ isPrime.js
@@ -1,11 +1,17 @@
-const isPrime = (n) => {
-    for(let i = 2; i < n ; i++) {
-        if(n%i === 0) {
+const isPrime = (number) => {
+    for(let i = 2; i < Math.sqrt(number) ; i++) {
+        if(number%i === 0) {
             return false;
         }
     }
     return true;
 }
+
+// Big O Best , Average , Worst Cases
+//Best Case: number = 1 OR number = 2 => O(1);
+//Average Case: O(n);
+//Worst Case: number = 100000 => O(n) but actually this is not the worst case.
+//If the number is decimal like 27.923 it will be the worst case in our isPrime() method; (Improved: O(sqrt(n)))
 
 console.log(isPrime(5));
 console.log(isPrime(6));
