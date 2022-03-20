6f6dae9aa823f09032006960179af333c7a26d1f
Jakhongirkhoja-Bakhodirov <masternuxtjstest@gmail.com>
2022-03-20 12:50:04 +0500

    Max solution with reduce method

--- first-practice.js
+++ first-practice.js
@@ -7,4 +7,34 @@ function sumArray(array) {
     return result;
 }
 
-console.log(sumArray([1,2,4]));
+// console.log(sumArray([1,2,4]));
+
+
+//Max solutions
+//example sumArray([1,3,5]);
+function sumArray(numbers) {
+    let result = 0;     // expression execute one times;
+    for(const number of numbers) {   // expression execute one times;
+        result+=number;  // expression execute three times;
+    }
+    return result;   // expression execute one times;
+
+    // return numbers.reduce((number , curNum) => sum , 0);
+}
+
+function sumViaReduce(numbers) {
+    return numbers.reduce((sum , currentNumber) => sum+currentNumber , 0);
+}
+
+// let start = 0 , end = 0;
+// start = performance.now();
+// sumArray([2,3,4]);
+// end = performance.now();
+// console.log(end-start);
+
+let start = 0 , end = 0;
+start = performance.now();
+sumViaReduce([2,2,3,4]);
+end = performance.now();
+console.log(end-start);
+// T = 1 + 1 + n + 1 = 3 + n;
